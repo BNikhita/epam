5d7a10dbf3c76baff609fa8aab48b21840db2c2e
harsh <harshithauriti@gmail.com>
2020-02-08 23:01:47 +0530

    commit

---
+++ example1/src/main/java/example1/Sweet1.java
@@ -0,0 +1,8 @@
+package example1;
+
+public abstract class Sweet1 {
+	public abstract int totalweight(int a[],int n);
+
+	public abstract int numofcandy(int low,int high,int a[],int n);
+
+}
---
+++ example1/src/main/java/example1/Sweet3.java
@@ -0,0 +1,42 @@
+package example1;
+
+public class Sweet3 extends Sweet1{
+	public int totalweight(int [] choco,int c)
+
+	{
+
+		int total=0;
+
+		for(int i=0;i<c;i++)
+
+		{
+
+			total=total+choco[i];
+
+		}
+
+		return total;
+
+	}
+
+	public int numofcandy(int low,int high,int[] sweets,int s)
+
+	{
+
+		int count=0;
+
+		for(int i=0;i<s;i++)
+
+		{
+
+			if(sweets[i]>=low && sweets[i]<=high)
+
+				count++;
+
+		}
+
+		return count;
+
+	}
+
+}
